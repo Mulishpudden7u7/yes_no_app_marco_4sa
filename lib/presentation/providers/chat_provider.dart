9fff4b68bd12c1d01c63cefc7065198a7b0c32be
Marco Velasco <marcovelas71@gmail.com>
2024-10-28 12:52:54 -0600

    Empezando Chatprovided

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app_marco_4sa/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> messageList = [
+    Message(text: "HOLA ADER", fromWho: FromWho.me),
+    Message(text: "¿LLORAS OOR QUE REPROBARAS?", fromWho: FromWho.me)
+  ];
+  //controlador para manejar el scroll
+  final ScrollController chatScrollController = ScrollController();
+
+  Future<void> sendMessage(String text) async {
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    //agrega un elemento a lista messagelist
+    messageList.add(newMessage);
+    //notifica si algo de provider cambio
+    notifyListeners();
+    //mueve el scroll
+    moveScrollToBottom();
+  }
+
+  Future<void> moveScrollToBottom() async {
+    //un pequeño atraso en la animacion para garantizar que siempre
+    //se vera aun cuando se envien mensajes cortos y rapido
+    await Future.delayed(const Duration(seconds: 1));
+    chatScrollController.animateTo(
+        //mover el scroll lo mas que se pueda, pociscion de la animacion
+        //offset :pocisicon de la animacion
+        //MaxcontrollerExtend primer linea
+        chatScrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut);
+  }
+}
